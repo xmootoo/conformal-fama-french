b809754b92a317a968dc8f88f923bd4611ed4c77
xmootoo <xmootoo@gmail.com>
2024-11-28 00:27:35 -0500

    Added config. Update with args later

---
+++ config.py
@@ -0,0 +1,12 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import os
+import yaml
+import time
+from datetime import datetime
+from pydantic import BaseModel, Field
+from typing import Set, Tuple, List, Optional, Dict, Union
+
+import random
+import string
